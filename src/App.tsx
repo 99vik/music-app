0cc39ac2bbe3113f2a679f734e35d20cd314c356
Viktor Miric <viktorm47@gmail.com>
2023-12-01 16:34:02 +0100

    Redux packages setup, api testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,39 @@
+import { useEffect, useState } from 'react';
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [song, setSong] = useState(null);
+
+  useEffect(() => {
+    const url =
+      'https://deezerdevs-deezer.p.rapidapi.com/search/?q=postmalone&limit=10&index=0';
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
+      },
+    };
+    async function getData() {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data);
+      const song = new Audio(data.data[0].preview);
+      setSong(song);
+    }
+    getData();
+  }, []);
+  return (
+    <>
+      <button
+        onClick={() => {
+          song.play();
+          console.log(song);
+        }}
+        className="bg-red-700"
+      >
+        Play
+      </button>
+    </>
+  );
 }
